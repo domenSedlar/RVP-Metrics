b97768c66ccde427d5e7f3f3091c15df59755235
domenSedlar <domen.sedlar@gmail.com>
2026-06-08 15:38:43 +0200

    Implemented moore stress measure

--- metrics.py
+++ metrics.py
@@ -1,7 +1,20 @@
 import numpy as np
 import cv2
 
-def conv(x, k, borderType=cv2.BORDER_CONSTANT):
+N4 = np.array([
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+], dtype=np.float64)
+
+N8 = np.array([
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1]
+    ], dtype=np.float64)
+    
+
+def conv(x, k, borderType=cv2.BORDER_ISOLATED):
     """
     Convolution on x with kernal k
     Parameters:
@@ -16,35 +29,63 @@ def me4(A):
     """
     Measure of effectivness using the 4 neighborhood
     """
-    W = np.array([
-        [0, 1, 0],
-        [1, 0, 1],
-        [0, 1, 0]
-    ], dtype=np.float64)
-    
-    return me(A, W)
+    return _me(A, N4)
 
 def me8(A):
     """
     Measure of effectivness using the 8 neighborhood
     """
-    W = np.array([
-        [1, 1, 1],
-        [1, 0, 1],
-        [1, 1, 1]
-    ], dtype=np.float64)
-    return me(A, W)
+    return _me(A, N8)
 
-def me(A, W):
+def _me(A, W):
     S = A*conv(A, W, borderType=cv2.BORDER_ISOLATED)
-    D = np.sum(S*S)
+    D = np.sum(S)
     
     return D
 
-if __name__ == "__main__":
-    data = np.loadtxt('./Data/small_random.tsv', delimiter='\t')
-    print(me4(data))
+def moore_stress4(A):
+    T = np.ones_like(A) * 4
+    (a,b) = T.shape
+    (a,b) = (a-1, b-1)
+    
+    T[:, 0] = 3
+    T[:, b] = 3
+    T[0, :] = 3
+    T[a, :] = 3
+    
+    T[0,0] = 2
+    T[0,b] = 2
+    T[a,0] = 2
+    T[a,b] = 2
+
+    print(T)
+
+    return(_moore_stress(A, N4, T))
+
+def moore_stress8(A):
+    T = np.ones_like(A) * 8
+    (a,b) = T.shape
+    (a,b) = (a-1, b-1)
+    
+    T[:, 0] = 5
+    T[:, b] = 5
+    T[0, :] = 5
+    T[a, :] = 5
+    
+    T[0,0] = 3
+    T[0,b] = 3
+    T[a,0] = 3
+    T[a,b] = 3
+
+    print(T)
+
+    return(_moore_stress(A, N8, T))
+
+def _moore_stress(A, k, T):
+    A2 = A * A
+    D = T * A2 - 2 * A * conv(A, k) + conv(A2, k)
+    return np.sum(D)
 
 if __name__ == "__main__":
     data = np.loadtxt('./Data/small_random.tsv', delimiter='\t')
-    print(me4(data))
+    print(moore_stress8(data))
