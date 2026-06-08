c24d5b7582cc9dca5533cfba275cdcf75bfadb46
domenSedlar <domen.sedlar@gmail.com>
2026-06-08 14:47:36 +0200

    Added me 8 neigborhood

--- metrics.py
+++ metrics.py
@@ -21,10 +21,29 @@ def me4(A):
         [1, 0, 1],
         [0, 1, 0]
     ], dtype=np.float64)
+    
+    return me(A, W)
+
+def me8(A):
+    """
+    Measure of effectivness using the 8 neighborhood
+    """
+    W = np.array([
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1]
+    ], dtype=np.float64)
+    return me(A, W)
+
+def me(A, W):
     S = A*conv(A, W, borderType=cv2.BORDER_ISOLATED)
     D = np.sum(S*S)
     
     return D
+
+if __name__ == "__main__":
+    data = np.loadtxt('./Data/small_random.tsv', delimiter='\t')
+    print(me4(data))
 
 if __name__ == "__main__":
     data = np.loadtxt('./Data/small_random.tsv', delimiter='\t')
